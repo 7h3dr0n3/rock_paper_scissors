5fb3df5688be810b1d75f592c2315a5ef1ac240e
dr0n3 <7h3dr0n3@gmail.com>
2022-12-19 20:09:25 +0530

    Update game() to calculate score

--- app.js
+++ app.js
@@ -33,12 +33,17 @@ function singleRound(playerSelection, computerSelection) {
 // console.log(singleRound(playerSelection, computerSelection));
 
 function game() {
+    let score = 0;
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt();
         const computerSelection = getComputerChoice();
         const round = i + 1;
         const results = singleRound(playerSelection, computerSelection);
+        if (results.includes('Win')) {
+            score += 1;
+        }
         console.log(`Round: ${round}: ` + results);
     }
+    console.log(`Your Score is ${score}`);
 }
 game();
