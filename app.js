db5289a412492cb0f9f3d47bcfab4202292d3967
dr0n3 <7h3dr0n3@gmail.com>
2022-12-28 19:58:48 +0530

    Fix app.js and updated index.js
    refractor and rewrite code to display running score and announce
    the winner of result once player score reaches 5 and hide buttons
    
    add new function displayScore(), declareWin() and hideButton()

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+//get compChoice using Math.random();
 const p = document.getElementById('compChoice');
 
 function getComputerChoice() {
@@ -13,63 +14,118 @@ function getComputerChoice() {
     }
 }
 
+let playerScore = 0, compScore = 0, round = 0;
+
+//play single round of game and return result
+
 function singleRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+
     if (playerSelection === "rock" && computerSelection === "scissor") {
+        round++;
+        ++playerScore;
+        displayScore(playerScore, compScore, round);
+
         return `"You Win! Rock crushed Scissor "`;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        round++;
+        ++playerScore;
+        displayScore(playerScore, compScore, round);
+
         return `"You Win! Rock could'nt crush Paper"`;
     } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        round++;
+        ++playerScore;
+        displayScore(playerScore, compScore, round);
+
         return `"You Win! Scissor cut's Paper"`;
     } else if (playerSelection === computerSelection) {
-        return "Play Again";
+        round++;
+        displayScore(playerScore, compScore, round);
+
+        return "Its Draw!, Play Again!";
     } else {
+        round++;
+        ++compScore;
+        displayScore(playerScore, compScore, round);
+
         return `You Lose! ${playerSelection} lost against ${computerSelection}`;
     }
 }
 
-
-// console.log(singleRound("rock", getComputerChoice()));
-
-// function game() {
-//     let score = 0;
-//     for (let i = 0; i < 5; i++) {
-// const playerSelection = prompt();
-// const computerSelection = getComputerChoice();
-//         const round = i + 1;
-// const results = singleRound(playerSelection, computerSelection);
-//         if (results.includes('Win')) {
-//             score += 1;
-//         }
-// console.log(`Round: ${round}: ` + results);
-//     }
-//     console.log(`Your Score is ${score}`);
-// }
-// game();
-
-function singleGame() {
-    this.addEventListener('click', (e) => {
-        const playerSelection = e.target.value;
-        const computerSelection = getComputerChoice();
-        // console.log(singleRound(playerSelection, computerSelection));
-        const playerChoice = document.querySelector('#player');
-        const compChoice = document.querySelector('#computer');
-        const result = document.querySelector('#result')
-        playerChoice.innerText = 'Your Choice: ' + playerSelection;
-        compChoice.innerText = 'Computer Choice: ' + computerSelection;
-        result.innerText = `${singleRound(playerSelection, computerSelection)}`;
-
-    });
-}
-
+// click listener for all choice buttons
 
 const button = document.querySelectorAll('button');
-button.forEach(e => singleGame());
+
+button.forEach(e => {
+    e.addEventListener('click', (e) => {
+        const playerSelection = e.target.value;
+        const computerSelection = getComputerChoice();
+
+        // display choice
+
+        const playerChoice = document.querySelector('#player');
+        const compChoice = document.querySelector('#computer');
+        playerChoice.innerText = 'Your Choice: ' + playerSelection;
+        compChoice.innerText = 'Computer Choice: ' + computerSelection;
+
+        // display result
+
+        const result = singleRound(playerSelection, computerSelection);
+        const resultSingleRound = document.querySelector('#result');
+        resultSingleRound.innerText = result;
+
+    });
+
+});
 
 
 
+// Display the running score
 
+function displayScore(playerScore, compScore, round) {
+    const roundDisplay = document.querySelector('#round');
+    const playerScoreDisplay = document.querySelector('#playerScore');
+    const compScoreDisplay = document.querySelector('#compScore');
+
+    roundDisplay.innerText = `Round : ${round}`;
+    playerScoreDisplay.innerText = `Your Score : ${playerScore}`;
+    compScoreDisplay.innerText = `Computer Score : ${compScore}`;
+
+    console.log(`Round: ${round}# Your Score: ${playerScore} and Computer Score: is ${compScore}`);
+
+    declareWin(playerScore, compScore, round);
+
+}
+
+//announce a winner of the game once one player reaches 5 points.
+function declareWin(playerScore, compScore) {
+
+    if (playerScore === 5 && compScore < 5) {
+        const winnerDisplay = document.querySelector('#winner');
+        winnerDisplay.innerText = `Congrats!, You Win Computer loses`;
+        hideButton();
+
+    } else if (compScore === 5 && playerScore < 5) {
+        const winnerDisplay = document.querySelector('#winner');
+        winnerDisplay.innerText = `Aw!, You Lose Computer Wins`;
+        hideButton();
+    }
+}
+
+//hide selection button and add reset button
+
+function hideButton() {
+    const selection = document.getElementById('selection');
+    selection.remove();
+    const reset = document.createElement('button');
+    reset.innerText = "Reset";
+    document.body.appendChild(reset);
+    reset.addEventListener('click', () =>
+        window.location.reload())
+}
 
 
 
